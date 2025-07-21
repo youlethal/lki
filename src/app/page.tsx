72410d583ec38ae20c39cef7e31b5ec98188d2e1
youlethal <78674543+youlethal@users.noreply.github.com>
2025-07-21 07:21:11 +0100

    Add src/app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import Footer from '@/components/Footer'
+import About from '@/components/About'
+import Gallery from '@/components/Gallery'
+import Contact from '@/components/Contact'
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <Hero />
+      <About />
+      <Gallery />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
